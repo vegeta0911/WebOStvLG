8d87a7ce5cb197f536a555396e38ad6da62a86e0
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2025-06-12 19:03:07 +0200

    Update WebOStvLG.class.php

--- core/class/WebOStvLG.class.php
+++ core/class/WebOStvLG.class.php
@@ -778,7 +778,7 @@ class WebOStvLG extends eqLogic {
             }   
         }
             
-            $cmd = system::getCmdSudo().' '.self::EXEC_LG.' --name "'.$lgtvscanin["list"][0]["tv_name"].'" --ssl getPowerState';
+            $cmd = 'timeout 10 '.system::getCmdSudo().' '.self::EXEC_LG.' --name "'.$lgtvscanin["list"][0]["tv_name"].'" --ssl getPowerState';
             $start = microtime(true);
             $lgtvinfo = shell_exec($cmd);
             $duration = microtime(true) - $start;
